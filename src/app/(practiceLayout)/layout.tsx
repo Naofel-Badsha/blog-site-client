b8110748348b615e5b7628259addae3508874887
Naofel-Badsha <naofelbadsha@gmail.com>
2026-01-29 10:53:07 +0600

    added third group

--- src/app/(practiceLayout)/layout.tsx
+++ src/app/(practiceLayout)/layout.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const PracticeLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <div>i
         <nav className='flex gap-10 m-8'>
-          
+          <Link href="/development" className='hover:underline'>Development</Link>
+          <Link href="/marketing" className='hover:underline'>Marketing</Link>
+          <Link href="/marketing/setting" className='hover:underline'>Setting</Link>
+          <Link href="/marmketing/setting/salse" className='hover:underline'>Selse</Link>
         </nav>
         {children}
     </div>
